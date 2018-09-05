6b8228ecb78748f5116b81204351f4416c73b052
Anuk Shakya <i11134@d2hs.com>
2018-09-05 05:20:01 -0700

    bbplus backend

---
+++ src/main/java/org/verscend/bbplus/repository/BloodRecordRepository.java
@@ -0,0 +1,8 @@
+package org.verscend.bbplus.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.verscend.bbplus.models.BloodRecord;
+
+public interface BloodRecordRepository extends CrudRepository<BloodRecord, Integer>{
+
+}
